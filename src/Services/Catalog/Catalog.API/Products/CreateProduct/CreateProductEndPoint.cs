941f087f25e62f5d6df97485eb4de3a873c30320
Roberto Frisanco <frisancoroberto@gmail.com>
2025-01-27 19:46:20 -0400

    add mapster and carter to buildingblocks lb

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,6 +1,21 @@
+using BuildingBlocks.CQRS;
+using Carter;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndPoint
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record CreateProdutResponse(Guid Id);
+
+public class CreateProductEndPoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        
+    }
 }
