4a6cdd8864692d362635031283a2d277b4716e66
Roberto Frisanco <frisancoroberto@gmail.com>
2025-02-02 18:11:25 -0400

    create route class with first operation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,6 +1,3 @@
-using BuildingBlocks.CQRS;
-using Carter;
-
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(
@@ -16,6 +13,21 @@ public class CreateProductEndPoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        
+        app.MapPost("/products", async
+                    (CreateProductRequest request, ISender sender) =>
+                {
+                    var command = request.Adapt<CreateProductCommand>();
+
+                    var result = await sender.Send(command);
+
+                    var response = result.Adapt<CreateProdutResponse>();
+
+                    return Results.Created($"/products/{response.Id}", response);
+                }
+            ).WithName("CreateProduct")
+            .Produces<CreateProdutResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/obj/Debug/net9.0/Catalog.API.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.API/obj/Debug/net9.0/Catalog.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4efbabe0e76a8a9cce531ce4e5c877cf1f9e48f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc59e3e6dd8201116854220d93cf772af3c0ed0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
